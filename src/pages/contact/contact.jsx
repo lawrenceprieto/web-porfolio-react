033d67e683c1878111f7cc72bb72e0e3a51e5249
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-02 11:05:53 +0800

    adjust font in contact page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
                     <div className="rectangle"></div>
                     <h1>CONTACT</h1>
                 </div>
-                <div className="contact-content">
+                <div className="contact-content fs-5">
                     I'm always open to collaboration and new opportunities. 
                     Feel free to reach out to me here, <a href="mailto:keithprietolawrence@gmail.com" target="_parent">keithprietolawrence@gmail.com</a>. 
                     Let's work together to turn ideas into reality.
